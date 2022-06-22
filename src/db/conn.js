54786f1194f1e0bb355dbd28fdcaff94b9c2869b
gaurverma <gaurverma333@gmail.com>
2022-06-22 09:20:29 +0530

    master

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
-const Port = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/FormRegistration";
-mongoose.connect(Port,{
+//const Port = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/FormRegistration";
+const DB = "mongodb+srv://vermagaur:raJesh%40567@cluster0.uzvy3.mongodb.net/nodejsintern?retryWrites=true&w=majority"
+mongoose.connect(DB,{
     useUnifiedTopology: true,
-    useNewUrlParser:true
+    useNewUrlParser:true,
 }).then(()=>{
     console.log("connection succesfull");
 }).catch((e)=>{
