e5b54404f893489d7aaeda1f141a762da1e407dd
gaurverma <gaurverma333@gmail.com>
2022-06-23 17:12:19 +0530

    lol

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 //const Port = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/FormRegistration";
-const DB = "mongodb+srv://vermagaur:raJesh%40567@cluster0.uzvy3.mongodb.net/nodejsintern?retryWrites=true&w=majority"
+const DB = "mongodb+srv://vermagaur:Dev567@cluster0.uzvy3.mongodb.net/nodejsintern?retryWrites=true&w=majority"
 mongoose.connect(DB,{
     useUnifiedTopology: true,
     useNewUrlParser:true,
